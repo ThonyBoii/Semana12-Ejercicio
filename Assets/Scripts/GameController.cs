554e191630782ede940d9ab3f2fb7a2419355b91
ThonyBoii <thonyabarca1705@gmail.com>
2024-11-29 13:51:25 -0500

    player movement

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject playerPrefab;
+    void Start()
+    {
+        if(PhotonNetwork.IsConnectedAndReady && Player.LocalInstance == null)
+        {
+            PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0, 10, 0), Quaternion.identity);
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if(Player.LocalInstance == null)
+        {
+            PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0, 10, 0), Quaternion.identity);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviourPun
 {
-    // Start is called before the first frame update
-    void Start()
+    private static GameObject localInstance;
+
+    [SerializeField] private TextMeshPro playerNameText;
+
+    private Rigidbody rb;
+    [SerializeField] private float speed;
+
+    public static GameObject LocalInstance { get { return localInstance;  } }
+
+    private void Awake()
     {
-        
+        if (photonView.IsMine)
+        {
+            playerNameText.text = GameData.playerName;
+            localInstance = gameObject;
+        }
+        DontDestroyOnLoad(gameObject);
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!photonView.IsMine || !PhotonNetwork.IsConnected)
+        {
+            return;
+        }
+        Move();
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector3(horizontal * speed, rb.velocity.y, vertical * speed);
+
+        if(horizontal!=0|| vertical != 0)
+        {
+            transform.forward = new Vector3(horizontal, 0, vertical);
+        }
+
     }
 }
