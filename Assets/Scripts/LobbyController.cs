1206918c6e67557046c2609d6cdff1158bd18a8f
ThonyBoii <thonyabarca1705@gmail.com>
2024-11-29 12:33:29 -0500

    go between scenes and connect to the server

--- Assets/Scripts/LobbyController.cs
+++ Assets/Scripts/LobbyController.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine.UI;
+using Photon.Pun.Demo.PunBasics;
 
-public class LobbyController : MonoBehaviour
+
+
+public class LobbyController : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button startButton;
+    [SerializeField] private TMP_InputField playerNameInputField;
+    [SerializeField] private string sceneName;
+
+    private void Awake()
     {
-        
+        PhotonNetwork.AutomaticallySyncScene = true;
+        startButton.onClick.AddListener(OnStartButtonClicked);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnStartButtonClicked()
     {
-        
+        GameData.playerName = playerNameInputField.text;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        RoomOptions options = new RoomOptions();
+        options.IsOpen = true;
+        options.IsVisible = true;
+        options.MaxPlayers = 4;
+
+        PhotonNetwork.JoinOrCreateRoom("Room", options, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.LoadLevel(sceneName);
+        }
     }
 }
